47386492de3f82babf7c81d508450e9bbbf365e1
Alon Horesh <57628667+AlonHor@users.noreply.github.com>
2025-11-09 14:34:46 +0200

    fun: add some drawing shapes for demo

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
-import { Button, Text, View } from "react-native";
-import { useFrequencyListener } from "../hooks/useFrequencyListener";
-import { useSineWavePlayer } from "../hooks/useSineWavePlayer";
-import { playNumbers } from "../utils/numberPlayer";
+import { useFrequencyListener } from "@/hooks/useFrequencyListener";
+import { useSineWavePlayer } from "@/hooks/useSineWavePlayer";
+import { playNumbers } from "@/utils/numberPlayer";
+import { Button, View } from "react-native";
 
 export default function Index() {
   const { playSequence } = useSineWavePlayer();
   const { listenFrequencies } = useFrequencyListener();
 
   function playSomeNumbers() {
-    playNumbers(playSequence, [12, 6, 32, 76, 16, 453, 43, 982]);
+    //playNumbers(playSequence, [12, 6, 736, 76, 16, 453, 43, 982]); // random numbers
+    //playNumbers(playSequence, [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]); // bits going up
+    //playNumbers(playSequence, [80, 136, 260, 514, 1025, 0, 0, 260, 260, 260, 260]); // smiley
+    playNumbers(playSequence, [260, 260, 508, 508, 248, 903, 903]); // creeper
   }
 
   function listenForNumbers() {
@@ -23,7 +26,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
       <Button onPress={playSomeNumbers} title="Start Playing" />
       <Button onPress={listenForNumbers} title="Start Listening" />
     </View>
